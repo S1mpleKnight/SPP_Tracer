5680d6f223d35a4629eaa8e6efb8888f83a0a32d
S1mpleKnight <become.adult77@gmail.com>
2021-11-02 03:02:17 +0300

    new: crete tracer entities

---
+++ TracerLib/Tracer.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+using System.Threading;
+
+namespace TracerLib
+{
+    public class Tracer : ITracer
+    {
+        private TraceResult traceResult;
+
+        public Tracer()
+        {
+            traceResult = new TraceResult();
+        }
+
+        public void StartTrace()
+        {
+            MethodBase currentMethod = new StackTrace().GetFrame(1).GetMethod();
+            traceResult.StartTrace(Thread.CurrentThread.ManagedThreadId, currentMethod);
+        }
+
+        public void StopTrace()
+        {
+            traceResult.StopTrace(Thread.CurrentThread.ManagedThreadId);
+        }
+
+        public TraceResult GetTraceResult()
+        {
+            return traceResult;
+        }
+    }
+}
---
+++ TracerLib/TracerResult.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Reflection;
+
+namespace TracerLib
+{
+    [Serializable]
+    [DataContract]
+    public class TraceResult
+    {
+        private ConcurrentDictionary<int, ThreadInfo> threadsList;
+        [DataMember(Name = "threads")]
+        public List<ThreadInfo> threads
+        {
+            get
+            {
+                SortedDictionary<int, ThreadInfo> sorteddictionary = new SortedDictionary<int, ThreadInfo>(threadsList);
+                return new List<ThreadInfo>(sorteddictionary.Values);
+            }
+        }
+
+        public TraceResult()
+        {
+            threadsList = new ConcurrentDictionary<int, ThreadInfo>();
+        }
+
+        public void StartTrace(int id, MethodBase method)
+        {
+            ThreadInfo threadInfo = threadsList.GetOrAdd(id, new ThreadInfo(id));
+            threadInfo.StartTrace(new MethodInfo(method));
+        }
+
+        public void StopTrace(int id)
+        {
+            ThreadInfo threadInfo;
+            if (!threadsList.TryGetValue(id, out threadInfo))
+            {
+                throw new ArgumentException("Invalid thread ID");
+            }
+            threadInfo.StopTrace();
+        }
+    }
+}
