0033473b0fefc08a6dfe99d8391d0396fa930eab
S1mpleKnight <become.adult77@gmail.com>
2021-11-02 03:05:25 +0300

    new: create console example

---
+++ SPP_Tracer/Examlpes.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using TracerLib;
+
+namespace LR1_SPP
+{
+    public class Examples
+    {
+        private ITracer tracer;
+
+        public Examples(ITracer tracer)
+        {
+            this.tracer = tracer;
+        }
+
+        public void StartTest()
+        {
+            var threads = new List<Thread>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Thread thread;
+                switch (i)
+                {
+                    case 0:
+                    case 1:
+                        thread = new Thread(firstMethod);
+                        break;
+                    case 2:
+                    case 3:
+                        thread = new Thread(secondMethod);
+                        break;
+                    default:
+                        thread = new Thread(sixthMethod);
+                        break;
+                }
+                threads.Add(thread);
+                thread.Start();
+            }
+
+            foreach (var thread in threads)
+            {
+                thread.Join();
+            }
+        }
+
+        public void firstMethod()
+        {
+            tracer.StartTrace();
+            Thread.Sleep(100);
+            thirdMethod();
+            tracer.StopTrace();
+        }
+
+        public void secondMethod()
+        {
+            tracer.StartTrace();
+            var threads = new List<Thread>();
+
+            for (int i = 0; i < 3; i++)
+            {
+                Thread thread = new Thread(fifthMethod);
+                threads.Add(thread);
+                thread.Start();
+            }
+
+            foreach (var thread in threads)
+            {
+                thread.Join();
+            }
+            Thread.Sleep(200);
+            fourthMethod();
+            fifthMethod();
+            tracer.StopTrace();
+        }
+
+        public void thirdMethod()
+        {
+            tracer.StartTrace();
+            Thread.Sleep(300);
+            tracer.StopTrace();
+        }
+
+        public void fourthMethod()
+        {
+            tracer.StartTrace();
+            Thread.Sleep(400);
+            tracer.StopTrace();
+        }
+
+        public void fifthMethod()
+        {
+            tracer.StartTrace();
+            var threads = new List<Thread>();
+
+            for (int i = 0; i < 2; i++)
+            {
+                Thread thread = new Thread(sixthMethod);
+                threads.Add(thread);
+                thread.Start();
+            }
+
+            foreach (var thread in threads)
+            {
+                thread.Join();
+            }
+            Thread.Sleep(500);
+            sixthMethod();
+            tracer.StopTrace();
+        }
+
+        public void sixthMethod()
+        {
+            tracer.StartTrace();
+            Thread.Sleep(600);
+            tracer.StopTrace();
+        }
+    }
+}
---
+++ SPP_Tracer/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using TracerLib;
+
+namespace LR1_SPP
+{
+    class Program
+    {
+        static Tracer tracer = new Tracer();
+
+        static void Main(string[] args)
+        {
+            TraceResult result;
+            Examples example = new Examples(tracer);
+            example.StartTest();
+            result = tracer.GetTraceResult();
+
+            string jsonFileName = "json.dat";
+            string xmlFileName = "xml.dat";
+
+            LoadToFile(new JsonConverter(), result, jsonFileName);
+            LoadToFile(new XMLConverter(), result, xmlFileName);
+            LoadToConsole(new JsonConverter(), result);
+        }
+
+        static void LoadToFile(IConverter converter, TraceResult result, string fileName)
+        {
+            IWriter writer = new FileWriter(fileName);
+            writer.Write(result, converter);
+        }
+
+        static void LoadToConsole(IConverter converter, TraceResult result)
+        {
+            IWriter writer = new ConsoleWriter();
+            writer.Write(result, converter);
+        }
+    }
+}
